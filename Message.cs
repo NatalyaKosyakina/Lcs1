507bce68d8d80f10f0c564553b7a069b6fe04533
natalya_kosyakina <anckamarckowa@yandex.ru>
2023-12-26 08:28:19 +0300

    homework

---
+++ Message.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace Lcs1
+{
+    internal class Message
+    {
+        public string Name {  get; set; }
+        public string Text { get; set; }
+
+        public DateTime Stime { get; set; }
+        
+        public string ToJson()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+        // 
+        public static Message? FromJson(string somemessage)
+        {
+            return JsonSerializer.Deserialize<Message>(somemessage);
+        }
+
+        public Message(string nikname, string text) { 
+            this.Name = nikname;
+            this.Text = text;
+            this.Stime = DateTime.Now;
+        }
+
+        public Message() { }
+
+        // Что скопировать и куда?
+        public override string ToString()
+        {
+            return $"Получено сообщение от {Name} ({Stime.ToShortTimeString()}): \n {Text}";
+        }
+    }
+}
